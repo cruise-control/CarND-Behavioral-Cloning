66df71a4fb2a3335c4e600777b41054a3420db15
S.C <>
2017-01-17 08:40:09 -0800

    Added a data generator

--- Clone.py
+++ Clone.py
@@ -13,6 +13,7 @@ from keras.models import model_from_json
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers import Convolution2D, MaxPooling2D
 from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.preprocessing.image import ImageDataGenerator
 from keras.optimizers import SGD, Adam, RMSprop
 from keras.utils import np_utils
 import random
@@ -79,7 +80,7 @@ class RawDataHandler:
         images_from_car /= 255
         images_from_car -= np.mean(images_from_car)
         images_from_car = self.pre_process_images(images_from_car)
-        return  np.asarray(images_from_car)
+        return np.asarray(images_from_car)
 
     def get_steering_angles(self):
         return self.csv_data.icol(self.csv_headers['steering_angle'])
@@ -126,10 +127,63 @@ class CloningModel:
         return model
 
 
+def train_flow(X_train, y_train, X_val, y_val):
+    batch_size = 32
+    nb_epoch = 2
+
+    train_datagen = ImageDataGenerator(
+        rotation_range=15,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        fill_mode='nearest',
+        horizontal_flip=False)
+
+    train_generator = train_datagen.flow(X_train, y_train, batch_size=batch_size)
+
+    cl = CloningModel()
+
+    # Figure out the input shape of the images
+    input_shape = X_data[1].shape
+
+    # Get the model to train on
+    clone_model = cl.BehaviorModel(input_shape=input_shape)
+    print(X_train.shape)
+
+    history = clone_model.fit_generator(train_generator, samples_per_epoch=2000,
+                                        nb_epoch=nb_epoch, validation_data=(X_val, y_val)
+                                        )
+    scr = clone_model.evaluate(X_val, y_val, batch_size=batch_size)
+    # cl.SaveMoodel(clone_model)
+    print("Final Score (on validation data is: ", scr)
+
+
+def train(X_train, y_train, X_val, y_val):
+    batch_size = 32
+    nb_epoch = 2
+
+    cl = CloningModel()
+
+    # Figure out the input shape of the images
+    input_shape = X_data[1].shape
+
+    # Get the model to train on
+    clone_model = cl.BehaviorModel(input_shape=input_shape)
+    print(X_train.shape)
+
+    history = clone_model.fit(X_train, y_train,
+                              batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(X_val, y_val),
+                              shuffle='true')
+    scr = clone_model.evaluate(X_val, y_val, batch_size=batch_size)
+    # cl.SaveMoodel(clone_model)
+    print("Final Score (on validation data is: ", scr)
+
+
 def test_on_images(mdl, X_val, y_val):
     print(len(y_val))
     print(X_val.shape)
-    for index in range(0,len(y_val)):
+    for index in range(0, len(y_val)):
         test_image = X_val[index]
         test_result = y_val[index]
         transformed_image_array = test_image[None, :, :, :]
@@ -138,35 +192,48 @@ def test_on_images(mdl, X_val, y_val):
 
 
 if __name__ == '__main__':
-    batch_size = 32
-    nb_epoch = 20
+
     train_flag = 1
 
     # Load the data ( place it into a pickle file if it is not already for future runs )
     raw_access = RawDataHandler("./simulator/driving_log.csv")
-    X_data, y_data = raw_access.get_pickle_data(location='center',regenerate='false')
+    X_data, y_data = raw_access.get_pickle_data(location='center', regenerate='false')
 
     # Split into train and validation sets
     X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=0.33, random_state=50)
 
-    clone = CloningModel()
-
     if train_flag:
-        # Figure out the input shape of the images
-        input_shape = X_data[1].shape
+        train_flow(X_train, y_train, X_val, y_val)
 
-        # Get the model to train on
-        clone_model = clone.BehaviorModel(input_shape=input_shape)
-        print(X_train.shape)
+    # batch_size = 32
+    # nb_epoch = 20
+    #
+    #
+    # # Load the data ( place it into a pickle file if it is not already for future runs )
+    # raw_access = RawDataHandler("./simulator/driving_log.csv")
+    # X_data, y_data = raw_access.get_pickle_data(location='center',regenerate='false')
+    #
+    # # Split into train and validation sets
+    # X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=0.33, random_state=50)
+    #
+    # clone = CloningModel()
+    #
+    # if train_flag:
+    #     # Figure out the input shape of the images
+    #     input_shape = X_data[1].shape
+    #
+    #     # Get the model to train on
+    #     clone_model = clone.BehaviorModel(input_shape=input_shape)
+    #     print(X_train.shape)
+    #
+    #     history = clone_model.fit(X_train, y_train,
+    #                               batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(X_val, y_val),
+    #                               shuffle='true')
+    #     score = clone_model.evaluate(X_val, y_val, batch_size=batch_size)
+    #     clone.SaveMoodel(clone_model)
+    #     print("Final Score (on validation data is: ", score)
 
-        history = clone_model.fit(X_train, y_train,
-                                  batch_size=batch_size, nb_epoch=nb_epoch, verbose=1, validation_data=(X_val, y_val),
-                                  shuffle='true')
-        score = clone_model.evaluate(X_val, y_val, batch_size=batch_size)
-        clone.SaveMoodel(clone_model)
-        print("Final Score (on validation data is: ", score)
-
-    test_on_images(clone.LoadModel(), X_val, y_val)
+    test_on_images(CloningModel.LoadModel(), X_val, y_val)
 
     # Train the model
     print('Finished')
