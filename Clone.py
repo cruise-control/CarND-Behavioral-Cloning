b3b80f0be5ef1d900f4df3864add0b46c64723bb
S.C <>
2017-01-20 15:10:34 -0800

    refactoring

--- Clone.py
+++ Clone.py
@@ -1,25 +1,15 @@
-from idlelib.idle_test.test_io import S
 
-import os.path, getopt
-import sys
-import pandas as pd
-import pickle
-import numpy as np
-from scipy.misc import imread, imresize
-# import matplotlib as plt
-from sklearn.model_selection import train_test_split
-# import math
-from keras.models import Sequential
-from keras.models import model_from_json
-from keras.layers.core import Dense, Dropout, Activation
-from keras.layers import Convolution2D, MaxPooling2D, ELU, Lambda
-from keras.layers import Dense, Dropout, Activation, Flatten
-from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import SGD, Adam, RMSprop
-from keras.utils import np_utils
 import random
 import argparse
-import gc
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.models import model_from_json
+from keras.layers import Convolution2D, MaxPooling2D, ELU, Dense, Dropout, Activation, Flatten
+from keras.preprocessing.image import ImageDataGenerator
+from scipy.misc import imread, imresize
+from sklearn.model_selection import train_test_split
 
 
 class Parameters:
@@ -46,7 +36,7 @@ class Parameters:
         print("raw_samples ", self.raw_samples)
 
     def parse_parameters(self):
-        parser = argparse.ArgumentParser(description='Pass arguemnts to the program')
+        parser = argparse.ArgumentParser(description='Pass arguments to the program')
         parser.add_argument('-t', '--test', help='Run the final tests', required=False, default=False)
         parser.add_argument('-T', '--train', help='Train the model', required=False, default=False)
         parser.add_argument('-e', '--epochs', type=int, help='number of epochs to run for', required=False, default=10)
@@ -130,108 +120,6 @@ class RawDataHandler:
         images_from_car -= np.mean(images_from_car)
         return images_from_car, np.asarray(y_values)
 
-        # def get_pickle_data(self, location='center', regenerate='False'):
-        #
-        #     self.pickle_raw_data(location=location, regenerate=regenerate)
-        #
-        #     return pickle.load(open(self.pickle_file, "rb"))
-        #
-        # def pickle_raw_data(self, location='center', regenerate='False'):
-        #
-        #     generate_pickled_file = 'False'
-        #
-        #     # Does the file exist? Yes? Are we forcing a regemeration? Np? Generate it
-        #     if os.path.isfile(self.pickle_file):
-        #         if regenerate == 'True':
-        #             generate_pickled_file = 'True'
-        #     else:
-        #         generate_pickled_file = 'True'
-        #
-        #     if generate_pickled_file == 'True':
-        #         x = self.get_images('center')
-        #         y = self.get_steering_angles()
-        #         pickle.dump((x, y), open(self.pickle_file, "wb"))
-        #         gc.collect()
-        #
-        #         xl = self.get_images('left')
-        #         yl = self.get_steering_angles()
-        #         yl *= self.side_steering_modifier
-        #         pickle.dump((xl, yl), open(self.pickle_file, "ab"))
-        #         gc.collect()
-        #
-        #         xr = self.get_images('right')
-        #         yr = self.get_steering_angles()
-        #         yr *= self.side_steering_modifier
-        #         pickle.dump((xr, yr), open(self.pickle_file, "ab"))
-        #         gc.collect()
-
-        # def get_image_locations(self, location="center"):
-        #
-        #     index = self.csv_headers[location]
-        #     image_paths = self.csv_data.icol(index)
-        #     image_paths = [x.strip() for x in image_paths]
-        #
-        #     return image_paths
-
-        # def pre_process_images(self, images):
-        #     result = list()
-        #     for img in images:
-        #         result.append(imresize(img, 50))
-        #     return result
-
-        # def get_images(self, location="center"):
-        #
-        #     image_locations = self.get_image_locations(location)
-        #     # set the offset for the images here
-        #     image_locations = ["./simulator/" + i for i in image_locations]
-        #     images_from_car = list()
-        #     # Load an image and reduce in size before adding to array
-        #     # This is to reduce the required memory
-        #     for img in image_locations:
-        #         image = imread(img).astype(np.float32)
-        #         image = imresize(image, 50).astype(np.float32)
-        #         images_from_car.append(image)
-        #     # Scale and order between -0.5 and 0.5
-        #     images_from_car = np.asarray(images_from_car)
-        #     images_from_car /= 255
-        #     images_from_car -= np.mean(images_from_car)
-        #     return images_from_car
-        #     # images_from_car = self.pre_process_images(images_from_car)
-        #     # return np.asarray(images_from_car)
-
-        # def pre_process_steering_angles(self, angles):
-        #     """
-        #     Ref: http://stackoverflow.com/questions/14313510/how-to-calculate-moving-average-using-numpy
-        #     :param angles:
-        #     :return:
-        #     """
-        #     # # Scale result to be between 0 - 50
-        #     # angles += 1
-        #     # # Convert to a float between 0 and 1
-        #     # angles /= 50
-        #
-        #     N = 5
-        #     # Perform an averaging pass over the data
-        #     angles = np.asarray(angles.tolist())
-        #
-        #     window = np.ones(int(N)) / float(N)
-        #
-        #     ret = np.convolve(a=angles, v=window, mode='SAME')
-        #
-        #     return ret
-
-        # def get_steering_angles(self):
-        #     """
-        #
-        #     :return:
-        #     """
-        #     angles = self.csv_data.icol(self.csv_headers['steering_angle'])
-        #
-        #     if self.smooth_steering:
-        #         return self.pre_process_steering_angles(angles)
-        #
-        #     return np.asarray(angles)
-
 
 class CloningModel:
     model_name = "model.json"
@@ -307,77 +195,73 @@ def train_flow_manual(input_shape, samples_to_load=1000, samples_per_epoch=5000,
         horizontal_flip=False,
         vertical_flip=False)
 
-    cl = CloningModel()
-
     # Get the model to train on
+    cl = CloningModel()
     clone_model = cl.get_model(input_shape=input_shape)
 
     for e in range(nb_epoch):
         print('Epoch', e)
 
-        # Load a fresh set of center images and 1/10th of random data from the total dataset here
-        X_data, y_data = image_access.get_data_set(location='center', nb_samples=samples_to_load)
-        X_data_r, y_data_r = image_access.get_data_set(location='random', nb_samples=int(samples_to_load / 40))
-        X_data = np.concatenate([X_data, X_data_r])
+        # Load a fresh set of center images and 1/40th of random Left or Right data from the total data-set
+        x_data, y_data = image_access.get_data_set(location='center', nb_samples=samples_to_load)
+        x_data_r, y_data_r = image_access.get_data_set(location='random', nb_samples=int(samples_to_load / 40))
+        x_data = np.concatenate([x_data, x_data_r])
         y_data = np.concatenate([y_data, y_data_r])
 
         # Split into train and validation sets
-        X_train, X_val, y_train, y_val = train_test_split(X_data, y_data, test_size=0.33, random_state=50)
+        x_train, x_val, y_train, y_val = train_test_split(x_data, y_data, test_size=0.33, random_state=50)
 
-        train_generator = train_datagen.flow(X_train, y_train, batch_size=batch_size)
+        train_generator = train_datagen.flow(x_train, y_train, batch_size=batch_size)
 
+        # Train for 1 epoch with this set of data
         clone_model.fit_generator(train_generator, samples_per_epoch=samples_per_epoch,
-                                  nb_epoch=1, validation_data=(X_val, y_val)
+                                  nb_epoch=1, validation_data=(x_val, y_val)
                                   )
 
-    X_test, y_test = image_access.get_data_set(location='center', nb_samples=200)
-    scr = clone_model.evaluate(X_test, y_test, batch_size=batch_size)
+    x_test, y_test = image_access.get_data_set(location='center', nb_samples=200)
+    scr = clone_model.evaluate(x_test, y_test, batch_size=batch_size)
     print("Final Score (on random test data) is: ", scr)
     cl.save_model(clone_model)
 
 
-def display_images(X_train, y_train):
-    # How many unique classes/labels there are in the dataset.
+def display_images(x_data, y_data):
+    # How many unique classes/labels there are in the data-set.
     y_value = set()
-    for y in y_train:
-        y_value.add(y)
+    for cls in y_data:
+        y_value.add(cls)
     n_classes = len(y_value)
-    import random
-    import matplotlib.pyplot as plt
-    import numpy as np
 
     for i in range(1):
         # From LeNet lab
-        index = random.randint(0, len(X_train))
-        image = X_train[index].squeeze()
+        index = random.randint(0, len(x_data))
+        image = x_data[index].squeeze()
         plt.figure(figsize=(5, 5))
-        plt.title('Class ' + str(y_train[index]))
+        plt.title('Class ' + str(y_data[index]))
         plt.imshow(image)
     y_data = [None] * n_classes
 
-    # Generate and plot a histogram of the dataset
-    for y in range(0, n_classes):
-        y_data[y] = 0
+    # Generate and plot a histogram of the data-set
+    for cls in range(0, n_classes):
+        y_data[cls] = 0
 
-    unique, unique_counts = np.unique(y_train, return_counts=True)
+    unique, unique_counts = np.unique(y_data, return_counts=True)
     for index in range(0, len(unique)):
         print(unique[index], " :: ", unique_counts[index])
 
-    idx = np.arange(n_classes)
     plt.figure(figsize=(20, 20))
     plt.title('Chart of classes in the dataset')
     plt.bar(unique, unique_counts, linewidth=0.1)
     plt.show()
 
 
-def test_on_images(X_val, y_val):
+def test_on_images(x_test, y_test):
     cln = CloningModel()
     mdl = cln.load_model()
-    print(len(y_val))
-    print(X_val.shape)
-    for index in range(0, len(y_val)):
-        test_image = X_val[index]
-        test_result = y_val[index]
+    print(len(y_test))
+    print(x_test.shape)
+    for index in range(0, len(y_test)):
+        test_image = x_test[index]
+        test_result = y_test[index]
         transformed_image_array = test_image[None, :, :, :]
         result = float(mdl.predict(transformed_image_array, batch_size=1))
         print("Result: ", result, " Expected: ", test_result)
@@ -394,12 +278,12 @@ if __name__ == '__main__':
     raw_access = RawDataHandler("./simulator/driving_log.csv")
 
     if params.train == 'True':
-        x, y = raw_access.get_data_set(2)
+        x, y = raw_access.get_data_set(nb_samples=2)
         train_flow_manual(x[0].shape, samples_to_load=5000, nb_epoch=params.epochs,
                           samples_per_epoch=params.samples_per_epoch, batch_size=params.batch_size)
 
     if params.test == 'True':
-        X_val, y_val = raw_access.get_data_set(200)
+        X_val, y_val = raw_access.get_data_set(nb_samples=200)
         test_on_images(X_val, y_val)
 
     # Train the model
